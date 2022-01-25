3f8f610a01e1ffd49b1bb4f0190cbda667409814
Igor Kharin <igkharin@gmail.com>
2022-01-25 18:32:12 +0300

    Finished 3rd task

---
+++ DynArray.java
@@ -0,0 +1,96 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class DynArray<T>
+{
+    public T [] array;
+    public int count;
+    public int capacity;
+    Class clazz;
+
+    public DynArray(Class clz)
+    {
+        clazz = clz;
+        count = 0;
+        makeArray(16);
+    }
+
+    public void makeArray(int new_capacity)
+    {
+        if (new_capacity >= 0) {
+            if(count == 0) {
+                array = (T[]) Array.newInstance(this.clazz, new_capacity);
+            } else  {
+                int j;
+                if (new_capacity < count) {
+                    j = new_capacity;
+                } else {
+                    j = count;
+                }
+                T [] array_copy = (T[]) Array.newInstance(this.clazz, new_capacity);
+                for (int i = 0; i<j; i++) {
+                    array_copy[i] = array[i];
+                }
+                array = array_copy;
+                count = j;
+            }
+        } else {
+            throw new IllegalArgumentException("");
+        }
+        capacity = new_capacity;
+    }
+
+    public T getItem(int index)
+    {
+        if (index >= 0 && index < count) {
+            return array[index];
+        }
+        else {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    public void append(T itm)
+    {
+        if (capacity < count + 1) {
+            this.makeArray(2 * capacity);
+        }
+        this.array[count] = itm;
+        count++;
+    }
+
+    public void insert(T itm, int index)
+    {
+        if (!(index >= 0 && index <= count)) {
+            throw new IllegalArgumentException("Illegal Argument: " + index);
+        }
+        if (capacity == count) {
+            this.makeArray(2 * capacity);
+        }
+        for (int i = count - 1; i > index; i--) {
+            array[i] = array[i-1];
+        }
+        array[index] = itm;
+        count++;
+        //O(n)
+    }
+
+    public void remove(int index)
+    {
+        if (!(index >= 0 && index <= count)) {
+            throw new IllegalArgumentException("Illegal Argument: " + index);
+        }
+
+        for (int i = index; i < count; i++) {
+            array[i] = array[i+1];
+        }
+        count--;
+        int new_capacity = (int) ((capacity * 1.0)/1.5);
+        if (count <= new_capacity) {
+            this.makeArray(new_capacity);
+        }
+        //O(n)
+    }
+
+}
---
+++ DynArrayTest.java
@@ -0,0 +1,114 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class DynArrayTest {
+    public DynArray<Integer> A = new DynArray<Integer>(Integer.class);
+    Random rand = new Random();
+
+    @Test
+    public void insert() {
+
+        // Вставка элемента, когда в итоге размер буфера не превышен (проверяем также размер буфера):
+        // Тестовые данные
+        A = new DynArray<Integer>(Integer.class);
+        for (int i = 0; i < 100; i++) {
+            A.append(rand.nextInt());
+        }
+        int buff = A.capacity; //128
+        int A_length = A.count; // 100
+        // Тест
+        A.insert(rand.nextInt(), rand.nextInt(100));
+        Assert.assertEquals(buff, A.capacity);
+        Assert.assertEquals(101,A.count);
+
+        // Вставка элемента, когда в результате превышен размер буфера
+        // Проверяем также корректное изменение размера буфера
+        // Тестовые данные
+        A = new DynArray<Integer>(Integer.class);
+        for (int i = 0; i < 128; i++) {
+            A.append(rand.nextInt());
+        }
+        buff = A.capacity; //128
+        A_length = A.count; // 128
+
+        //Тест
+        A.insert(rand.nextInt(), rand.nextInt(100));
+        Assert.assertEquals(2*buff, A.capacity);
+        Assert.assertEquals(129,A.count);
+
+    }
+
+    @Test
+    // Попытка вставки элемента в недопустимую позицию
+    public void InsertIllegalArgumentTest() throws IllegalArgumentException {
+        // Тестовые данные
+        A = new DynArray<Integer>(Integer.class);
+        for (int i = 0; i < 100; i++) {
+            A.append(rand.nextInt());
+        }
+        try {
+            A.insert(rand.nextInt(), A.count * rand.nextInt());
+            Assert.fail("Expected exception!");
+        } catch (IllegalArgumentException thrown) {
+            Assert.assertNotEquals("", thrown.getMessage());
+        }
+    }
+
+    @Test
+    public void remove() {
+
+        // удаление элемента, когда в результате размер буфера остаётся прежним
+        // проверяем также размер буфера
+
+        // Тестовые данные
+        A = new DynArray<Integer>(Integer.class);
+        for (int i = 0; i < 100; i++) {
+            A.append(rand.nextInt());
+        }
+        int buff = A.capacity; //128
+        int A_length = A.count; // 100
+
+        // Тест
+        A.remove(rand.nextInt(100));
+        Assert.assertEquals(99,A.count);
+        Assert.assertEquals(buff,A.capacity);
+
+
+        // удаление элемента, когда в результате понижается размер буфера
+        // проверяем также корректное изменение размера буфера
+        A = new DynArray<Integer>(Integer.class);
+        for (int i = 0; i < 86; i++) {
+            A.append(rand.nextInt());
+        }
+        buff = A.capacity; // 128
+        A_length = A.count; // 86
+
+        // Тест
+        A.remove(rand.nextInt(100));
+        Assert.assertEquals(85,A.count);
+        int new_buff = (int) ((buff * 1.0)/1.5);
+        Assert.assertEquals(new_buff, A.capacity);
+    }
+
+    @Test
+    // Попытка вставки элемента в недопустимую позицию
+    public void removeIllegalArgumentTest() throws IllegalArgumentException {
+        // Тестовые данные
+        A = new DynArray<Integer>(Integer.class);
+        for (int i = 0; i < 100; i++) {
+            A.append(rand.nextInt());
+        }
+        try {
+            A.remove(A.count * rand.nextInt());
+            Assert.fail("Expected exception!");
+        } catch (IllegalArgumentException thrown) {
+            Assert.assertNotEquals("", thrown.getMessage());
+        }
+    }
+}
