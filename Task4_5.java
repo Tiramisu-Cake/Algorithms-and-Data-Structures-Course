58df33f7e56d33f2b1f350c4b2958cae3b4cdb37
Igor Kharin <igkharin@gmail.com>
2022-01-27 23:37:02 +0300

    Corrected BracketBalance function

--- Task4_5.java
+++ Task4_5.java
@@ -3,32 +3,21 @@ package com.company;
 public class Task4_5 {
     // Сбалансированные строки
     public static boolean BracketBalance(String S) { // считаем, что строка состоит только из скобок или пустая
-        boolean res = true; // пустую строку считаем сбалансированной
-        Stack<Character> Stack = new Stack<>();
-        for (int i = 0; i < S.length(); i++) {
-            Stack.push(S.charAt(i)); // будем рассматривать строку с конца
+        if (S.length() % 2 != 0) { // если число символов нечетно, то проверять нечего
+            return false;
         }
-        Character C;
-        int close = 0;
-        while (Stack.size() > 0) {
-            C = Stack.pop();
-            // считаем закрывающие скобки до первой открывающей или до конца стека
-            while(C == Character.valueOf(')') || C == null) {
-                close++;
-                C = Stack.pop();
-            }
-            int open = 0;
-            // считаем открывающие скобки до первой закрывающей или до конца стека
-            while(C == Character.valueOf('(') || C == null) {
-                open++;
-                C = Stack.pop();
-            }
-            if (close == open) { // если открывающий и закрывающих одинаковое кол-во, то считаем дальше
-                close = 1;
-            }
-            else { // если нет, то строка не сбалансирована и дальше можно не проверять
-                res = false;
-                break;
+        boolean res = true; // пустую строку считаем сбалансированной
+        Stack<Character> Stack = new Stack();
+        for (int i = 0; i < S.length(); i++) { // идем по строке сначала
+            if (S.charAt(i) == Character.valueOf('(')) {
+                Stack.push(S.charAt(i)); // если скобка открывающая, добавляем в стек
+            } else { // если закрывающая, то проверяем стек
+                // если стек не пуст, то мы проверкой условия удаляем соответсвующую открывающую скобку
+                if (Stack.pop() != Character.valueOf('(')) {
+                    // если он пуст, то у закрывающей скобки не нашлось той, которая ее открывает
+                    res = false;
+                    break;
+                }
             }
         }
         return res;
