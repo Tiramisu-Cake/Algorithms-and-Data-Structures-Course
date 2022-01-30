7cc4dabffbe4b14d6d9de233ea784c30a340fdf1
Igor Kharin <igkharin@gmail.com>
2022-01-30 13:38:06 +0300

    Corrected Palindrom function

--- Task6_3.java
+++ Task6_3.java
@@ -2,12 +2,12 @@ package com.company;
 
 public class Task6_3 {
     public static boolean Palindrom(String S) {
-        Deque Q = new Deque();
+        Deque<Character> Q = new Deque();
         for (int i = 0; i < S.length(); i++) { // заполняем очередь строкой по-элементно
             Q.addTail(S.charAt(i));
         }
         for (int i = 0; i < S.length() / 2; i++) { // проверяем первый и последний элемент очереди на равенство
-            if ((char) Q.removeFront() != (char) Q.removeTail()) {
+            if (Q.removeFront().compareTo(Q.removeTail()) != 0) {
                 return false;
             }
         }
